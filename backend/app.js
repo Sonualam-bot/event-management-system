4dfa854e35fe15f8aede4ca24fa1357891e8c082
Sonualam <sonualam1599@gmail.com>
2023-10-31 16:04:13 +0530

    backend wiring done

---
+++ backend/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use(express.json());
+
+const eventRouter = require("./routes/event.route");
+
+app.get("/", (req, res) => {
+  res.send("Event Management System");
+});
+
+const corsOptions = {
+  origin: "*",
+};
+
+app.use(cors(corsOptions));
+
+app.use("/api/v1", eventRouter);
+
+module.exports = app;
